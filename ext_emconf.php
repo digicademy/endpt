a6cee0f126debd3252013d6794afab3b8fb19d46
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2025-07-23 01:16:06 +0200

    Change extension category

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title'          => 'ENDPT',
     'description'    => 'SPARQL, SRU, and OAI-PMH endpoints for TYPO3',
-    'category'       => 'misc',
+    'category'       => 'services',
     'author'         => 'Jonatan Jalle Steller',
     'author_email'   => 'jonatan.steller@adwmainz.de',
     'author_company' => 'Academy of Sciences and Literature Mainz',
